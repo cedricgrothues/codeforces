0917e308ba33f574e253827e82f7b961c5831424
Cedric Grothues <cedricgrothues@gmail.com>
2020-12-27 16:46:34 +0100

    Create 266/A.cpp

---
+++ 266/A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int n, m = 0;
+  cin >> n;
+  string s;
+  cin >> s;
+
+  for (int i = 0; i < n - 1; i++)
+    if (s[i] == s[i + 1])
+      m++;
+
+  cout << m;
+}
